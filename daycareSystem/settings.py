f08e227ecee2dcdaae00e36f1ccd3aefc9a97ae8
Srimal <srimalhs@gmail.com>
2024-08-31 10:01:47 +0530

    adding static root

--- daycareSystem/settings.py
+++ daycareSystem/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MEDIA_URL = "media/"
 
 
