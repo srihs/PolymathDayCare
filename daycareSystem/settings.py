9acf13c36588ecbea78e41cb59ae143eed0e3a84
Srimal <srimalhs@gmail.com>
2024-09-01 10:31:03 +0530

    fixing qr issue in settings.py

--- daycareSystem/settings.py
+++ daycareSystem/settings.py
@@ -185,7 +185,7 @@ CORS_ALLOW_METHODS = [
 
 LOGIN_URL = "core:login"
 
-PROD_URL = "https://dc.polymathcore.online"  # this is the URL that will stored in the QR URL for scanning
+PROD_URL = "https://dc.polymathcore.online/"  # this is the URL that will stored in the QR URL for scanning
 QR_METHOD_NAME = (
     "checkInView/"  # this is the method that will stored in the QR URL for scanning
 )
