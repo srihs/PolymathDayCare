336d004c9f155605d44343ae2297b72e3de8aa2a
Srimal Samarasooriya <srimalhs@gmail.com>
2025-06-13 15:30:00 +0530

    invoice initial commit

--- core/urls.py
+++ core/urls.py
@@ -319,4 +319,14 @@ urlpatterns = [
         views.generateInvoiceJS,
         name="generateinvoicesJS",
     ),
+    path(
+        "generate_invoice_pdf/",
+        views.generate_invoice_pdf,
+        name="generate_invoice_pdf",
+    ),
+    path(
+        "generate-clean-minimal-pdf/",
+        views.generate_clean_minimal_pdf,
+        name="generate_clean_minimal_pdf",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
